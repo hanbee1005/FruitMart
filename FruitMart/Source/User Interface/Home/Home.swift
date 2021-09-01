6cd46c32e77aee127f9a563934ee606198bba049
hanbee <sonhanbi1002@gmail.com>
2021-09-01 11:08:50 +0900

    초기 설정

---
+++ FruitMart/Source/User Interface/Home/Home.swift
@@ -0,0 +1,21 @@
+//
+//  Home.swift
+//  FruitMart
+//
+//  Created by 손한비 on 2021/09/01.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
