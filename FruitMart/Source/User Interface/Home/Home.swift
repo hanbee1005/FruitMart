39492825edc8975317f0648dfe448d3d8356f6ed
hanbee <sonhanbi1002@gmail.com>
2021-09-05 17:12:57 +0900

    상품 빠른 주문 기능 구현

--- FruitMart/Source/User Interface/Home/Home.swift
+++ FruitMart/Source/User Interface/Home/Home.swift
@@ -10,11 +10,13 @@ import SwiftUI
 struct Home: View {
     @EnvironmentObject private var store: Store
     
+    @State private var quickOrder: Product?  // 빠른 주문 기능으로 주문한 상품 저장
+    
     var body: some View {
         NavigationView {
             List(store.products) { product in
                 NavigationLink(destination: ProductDetailView(product: product)) {
-                    ProductRow(product: product)
+                    ProductRow(product: product, quickOrder: self.$quickOrder)
                 }
             }
             .navigationBarTitle("과일마트")
--- FruitMart/Source/User Interface/Home/ProductRow.swift
+++ FruitMart/Source/User Interface/Home/ProductRow.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ProductRow: View {
     let product: Product
     
+    @EnvironmentObject var store: Store
+    @Binding var quickOrder: Product?
+    
     var body: some View {
         HStack {
             productImage        // 상품 이미지
@@ -65,7 +68,15 @@ private extension ProductRow {
             Image(systemName: "cart")
                 .foregroundColor(.peach)
                 .frame(width: 32, height: 32)
+                .onTapGesture {
+                    self.orderProduct()
+                }
         }
+    }
+    
+    func orderProduct() {
+        quickOrder = product  // 주문 상품 저장. 팝업창 출력 조건
+        store.placeOrder(product: product, quantity: 1)  // 상품 1개 주문
     }
 }
 
@@ -73,10 +84,10 @@ struct ProductRow_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             ForEach(productSamples) {
-                ProductRow(product: $0)
+                ProductRow(product: $0, quickOrder: .constant(nil))
             }
             
-            ProductRow(product: productSamples[0])
+            ProductRow(product: productSamples[0], quickOrder: .constant(nil))
                 .preferredColorScheme(.dark)  // 다크 모드 설정
         }
         .padding()  // sizeThatFits를 이용하여 보더라도 약간의 여백을 주도록 추가
