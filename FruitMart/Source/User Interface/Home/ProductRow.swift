6ecc9476a3386ad6860a719c8bb7579793b5f3d7
hanbee <sonhanbi1002@gmail.com>
2021-09-06 09:58:30 +0900

    상품 목록이 나타날 때 페이드 인 / 아웃 효과 적용

--- FruitMart/Source/User Interface/Home/ProductRow.swift
+++ FruitMart/Source/User Interface/Home/ProductRow.swift
@@ -13,6 +13,8 @@ struct ProductRow: View {
     @EnvironmentObject var store: Store
     @Binding var quickOrder: Product?
     
+    @State private var willAppear: Bool = false
+    
     var body: some View {
         HStack {
             productImage        // 상품 이미지
@@ -23,6 +25,9 @@ struct ProductRow: View {
         .cornerRadius(6)
         .shadow(color: .primaryShadow,radius: 1, x: 2, y: 2)
         .padding(.vertical, 8)
+        .opacity(willAppear ? 1 : 0)
+        .animation(.easeInOut(duration: 0.4))
+        .onAppear { self.willAppear = true }
     }
 }
 
