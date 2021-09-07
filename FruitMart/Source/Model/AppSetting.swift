49b67b6f1669708a8151fb3ae5180d61c652d8d1
hanbee <sonhanbi1002@gmail.com>
2021-09-07 10:16:05 +0900

    설정 관련 모델 생성

---
+++ FruitMart/Source/Model/AppSetting.swift
@@ -0,0 +1,13 @@
+//
+//  AppSetting.swift
+//  FruitMart
+//
+//  Created by 손한비 on 2021/09/07.
+//
+
+import SwiftUI
+
+struct AppSetting {
+    var showFavoriteList: Bool = true
+    var productRowHeight: CGFloat = 150
+}
